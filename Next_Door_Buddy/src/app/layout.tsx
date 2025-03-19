71ffdf057142932e9f54687e9abc19f4d7113534
Lost4295 <turin-ylan@outlook.fr>
2025-03-19 10:51:53 +0100

    add theme change

--- Next_Door_Buddy/src/app/layout.tsx
+++ Next_Door_Buddy/src/app/layout.tsx
@@ -3,6 +3,9 @@ import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import {getPotentialUser} from "#/lib/dal";
+import {ThemeProvider} from "next-themes";
+import ThemeChooser from "#/components/ThemeChooser";
+
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -13,21 +16,6 @@ const geistMono = Geist_Mono({
     variable: "--font-geist-mono",
     subsets: ["latin"],
 });
-
-function textColor(color: string) {
-    let r, g, b;
-    if (color.length === 6) {
-        r = parseInt(color.slice(0, 2), 16);
-        g = parseInt(color.slice(2, 4), 16);
-        b = parseInt(color.slice(4, 6), 16);
-    } else {
-        r = parseInt(color.slice(0, 1).repeat(2), 16);
-        g = parseInt(color.slice(1, 2).repeat(2), 16);
-        b = parseInt(color.slice(2, 3).repeat(2), 16);
-    }
-    const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
-    return luminance > 0.5 ? "black" : "white";
-}
 
 export default async function RootLayout({
                                              children,
@@ -46,44 +34,50 @@ export default async function RootLayout({
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-        <nav className={"bg-gray-100"}>
-            <div className="py-4 flex">
-                <Link className="text-3xl ml-5" href="/">
-                    La Porte à côté
-                </Link>
-                <div className="flex justify-around grow items-center" id="navbarSupportedContent">
-                    <Link className=" text-blue-600 visited:text-purple-600" href="/">
-                        Home
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <nav className={"bg-gray-100"}>
+                <div className="py-4 flex">
+                    <Link className="text-3xl ml-5" href="/">
+                        La Porte à côté
                     </Link>
-                    <Link className=" text-blue-600 visited:text-purple-600" href="/create">
-                        Create
-                    </Link>
-                    <Link className=" text-blue-600 visited:text-purple-600" href="/see">
-                        See
-                    </Link>
-                    {user ? (
-                        <>
-                            <Link
-                                className=" text-blue-600 visited:text-purple-600"
-                                href={"/profile/" + user.userId + "/results"}
-                            >
-                                Results
-                            </Link>
-                            <Link href={"/profile"} className={" text-blue-600 visited:text-purple-600"}>
-                                Profile
-                            </Link>
-                            <Link href={"/games"} className={" text-blue-600 visited:text-purple-600"}>
-                                Games
-                            </Link>
-                            <Link href={"/logout"} className={" text-blue-600 visited:text-purple-600"}>
-                                Logout
-                            </Link>
-                        </>
-                    ) : (
-                        <>
-                            <Link className=" text-blue-600 visited:text-purple-600" href="/login">
-                                Connect
-                            </Link>
+                    <div className="flex justify-around grow items-center" id="navbarSupportedContent">
+                        <Link className=" text-blue-600 visited:text-purple-600" href="/">
+                            Home
+                        </Link>
+                        <Link className=" text-blue-600 visited:text-purple-600" href="/create">
+                            Create
+                        </Link>
+                        <Link className=" text-blue-600 visited:text-purple-600" href="/see">
+                            See
+                        </Link>
+                        {user ? (
+                            <>
+                                <Link
+                                    className=" text-blue-600 visited:text-purple-600"
+                                    href={"/profile/" + user.userId + "/results"}
+                                >
+                                    Results
+                                </Link>
+                                <Link href={"/profile"} className={" text-blue-600 visited:text-purple-600"}>
+                                    Profile
+                                </Link>
+                                <Link href={"/games"} className={" text-blue-600 visited:text-purple-600"}>
+                                    Games
+                                </Link>
+                                <Link href={"/logout"} className={" text-blue-600 visited:text-purple-600"}>
+                                    Logout
+                                </Link>
+                            </>
+                        ) : (
+                            <>
+                                <Link className=" text-blue-600 visited:text-purple-600" href="/login">
+                                    Connect
+                                </Link>
 
                             <Link className="text-blue-600 visited:text-purple-600" href="/register">
                                 Register
@@ -98,6 +92,21 @@ export default async function RootLayout({
             <div className="w-full"><p className="m-0 text-center text-black-600 ">Copyright © Your Website 2023</p>
             </div>
         </footer>
+                                <Link className="text-blue-600 visited:text-purple-600" href="/register">
+                                    Register
+                                </Link>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </nav>
+                {children}
+            <ThemeChooser/>
+            <footer className="bg-gray-100 py-4">
+                <div className="w-full"><p className="m-0 text-center text-black-600 ">Copyright © Your Website 2023</p>
+                </div>
+            </footer>
+        </ThemeProvider>
         </body>
         </html>
     );
