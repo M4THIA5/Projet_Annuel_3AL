23f48b07c3e25f1cfde8aab951a2f71647a54353
Lost4295 <turin-ylan@outlook.fr>
2025-05-14 17:44:15 +0200

    work: update email creation update some parameters for email creation (still needs preparation)

--- Next_Door_Buddy/src/emails/send.tsx
+++ Next_Door_Buddy/src/emails/send.tsx
@@ -3,20 +3,20 @@ import nodemailer from 'nodemailer'
 import {Email} from './email'
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.forwardemail.net',
+    host: 'smtp.ionos.fr',
     port: 465,
     secure: true,
     auth: {
-        user: 'my_user',
-        pass: 'my_password',
+        user: 'noreply@laporteacote.online',
+        pass: 'PWqKMnpN*XF0Z%naO8l7',
     },
 })
 
 const emailHtml = await render(<Email url="https://example.com"/>)
 
 const options = {
-    from: 'you@example.com',
-    to: 'user@gmail.com',
+    from: 'noreply@laporteacote.online',
+    to: 'adetrindade@myges.fr',
     subject: 'hello world',
     html: emailHtml,
 }
