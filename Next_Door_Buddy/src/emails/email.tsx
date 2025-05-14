3be7eb8f40a3498eba25c1f82979365c8df081df
Lost4295 <turin-ylan@outlook.fr>
2025-05-14 15:16:18 +0200

    work: add email creation add some code to send an email (still needs preparation)

---
+++ Next_Door_Buddy/src/emails/email.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import {Button, Html} from "@react-email/components"
+
+export function Email(props: { url: string }) {
+    const {url} = props
+
+    return (
+        <Html lang="en">
+            <Button href={url}>Click me</Button>
+        </Html>
+    )
+}
