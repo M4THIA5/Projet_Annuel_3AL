fdd196044e67c3a794342da6fd5ccb76a11ca81a
Lost4295 <turin-ylan@outlook.fr>
2025-06-06 11:36:58 +0200

    dev: add api key to plugin since Gif plugin needs a api key, its now provided with the plugin ! (poorly)

--- JavaWebScraper/plugins/gif/src/main/java/pa/gifplugin/GifPlugin.java
+++ JavaWebScraper/plugins/gif/src/main/java/pa/gifplugin/GifPlugin.java
@@ -1,9 +1,14 @@
 package pa.gifplugin;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.pf4j.Plugin;
 import org.pf4j.PluginWrapper;
 
 import javax.swing.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Objects;
 
 public class GifPlugin extends Plugin {
 
@@ -15,6 +20,23 @@ public class GifPlugin extends Plugin {
     public void start() {
         System.out.println("GifPlugin started!");
         String[] a = new String[1];
+        String decodedKey;
+        try {
+            byte[] decodedBytes = Base64.getDecoder().decode(Objects.requireNonNull(
+                    getClass().getClassLoader().getResourceAsStream("k")
+            ).readAllBytes());
+            decodedKey = new String(decodedBytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(".env", true));
+            writer.append("\nGIPHY_KEY=\"").append(decodedKey).append("\"");
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         main(a);
     }
 
@@ -24,7 +46,7 @@ public class GifPlugin extends Plugin {
     }
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "GifPlugin is now active.","Information",  JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "GifPlugin is now active.", "Information", JOptionPane.INFORMATION_MESSAGE);
     }
 }
 
