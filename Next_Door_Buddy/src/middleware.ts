ba151b26b706d59447ea1b6c81a3c7f3a4e5711b
Ylan <turin-ylan@outlook.fr>
2025-07-10 11:39:26 +0200

    dev: add a return_to just some redirection after login

--- Next_Door_Buddy/src/middleware.ts
+++ Next_Door_Buddy/src/middleware.ts
@@ -14,9 +14,9 @@ export async function middleware(req: NextRequest) {
   const isAuth = await isAuthenticated()
   const isPublicRoute = PUBLIC_ROUTES.some((route) => req.nextUrl.pathname.startsWith(route))
   const isAdminRoute = req.nextUrl.pathname.startsWith(Routes.admin.toString())
-
   if (!isAuth && !isPublicRoute) {
-    return NextResponse.redirect(new URL(Routes.auth.login.toString(), req.url))
+    const pls = req.nextUrl.pathname.replace('/','').length > 0 ? "?return_to="+ req.nextUrl.pathname.replace('/',''): ''
+    return NextResponse.redirect(new URL(Routes.auth.login.toString()+pls, req.url))
   }
   if (isAuth && isPublicRoute) {
     return NextResponse.redirect(new URL(Routes.home.toString(), req.url))
